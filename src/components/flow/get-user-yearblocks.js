f5bea1ba0126eda3d359401337b3a1dbf7b33404
sky cherfa <sherifcherfa@gmail.com>
2023-08-15 07:02:39 -0400

    display data

--- src/components/flow/get-user-yearblocks.js
+++ src/components/flow/get-user-yearblocks.js
@@ -1,42 +1,56 @@
 import * as fcl from "@onflow/fcl";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import OverlayLoading from "components/OverlayLoading";
 
 fcl.authenticate();
-function ViewYearblock({ className }) {
+function ViewYearblock() {
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({ loggedIn: false, addr: undefined })
 
+	useEffect(() => {
+    fcl.currentUser.subscribe(setUser);
+  }, [user.addr]);
+  console.log(user.addr)
   //user address
-  async function displayYearblock() {
-    const user = fcl.currentUser().authorization;
-    console.log(user, "user");
+  async function displayYearblock(addr) {
     setLoading(true);
     try {
       const res = await fcl.query({
         cadence: `
-            import "YearBlocks" from 0x770b3ddf7db51dd1
+            import YearBlocks from 0x770b3ddf7db51dd1
             import NonFungibleToken from 0x770b3ddf7db51dd1
+
+            pub struct MetaDataStruct {
+              pub var name: String?
+              pub var allowList: [String]?
+              pub var link: String?
             
+              init(_ id: UInt64, _ NFT: &YearBlocks.NFT?) {
+            
+                self.name = NFT?.getName()
+                self.allowList = NFT?.getAllowList()
+                self.link = NFT?.getLink()
+              }
+            }
             /// This script returns the IDs of the KittyVerse NFTs in the Collection of the given Address
-              ///
-              pub fun main(address: Address): {UInt64: String?} {
-              
-                  var idsToNames: {UInt64: String?} = {}
-                  // Get a reference to the CollectionPublic Capability from the specified Address
-                  let collectionPublicRef = getAccount(address).getCapability<&{YearBlocks.CollectionPublic}>(
-                    YearBlocks.CollectionPublicPath 
-                  ).borrow()
-                  ?? panic("Couldn't find CollectionPublic Capability at given Address!")
-                  let ids = collectionPublicRef.getIDs()
-                  for id in ids {
-                    let NFT: &YearBlocks.NFT? = collectionPublicRef.borrowYearBlockNFT(id: id)
-                    let name = NFT?.getName()
-                    idsToNames.insert(key: id, name)
-                  }
-              
-                  return idsToNames
-              }`,
+            ///
+            pub fun main(address: Address): {UInt64: MetaDataStruct} {
+            
+                var nftDataMap: {UInt64: MetaDataStruct} = {}
+                // Get a reference to the CollectionPublic Capability from the specified Address
+                let collectionPublicRef = getAccount(address).getCapability<&{YearBlocks.CollectionPublic}>(
+                  YearBlocks.CollectionPublicPath 
+                ).borrow()
+                ?? panic("Couldn't find CollectionPublic Capability at given Address!")
+                let ids = collectionPublicRef.getIDs()
+                for id in ids {
+                  let NFT: &YearBlocks.NFT? = collectionPublicRef.borrowYearBlockNFT(id: id)
+                  nftDataMap[id] = MetaDataStruct(id, NFT)
+                }
+            
+                return nftDataMap
+            }`,
           args: (arg, t) => [arg(user.addr, t.Address)],
       });
       console.log(res);
@@ -54,7 +68,6 @@ function ViewYearblock({ className }) {
       {loading && <OverlayLoading />}
       <>
         <button
-          className={className}
           onClick={() =>
             displayYearblock()
           }
--- src/views/MyYearBlock/MyYearBlock.tsx
+++ src/views/MyYearBlock/MyYearBlock.tsx
@@ -1,12 +1,14 @@
 import Layout from "components/Layout";
 import Hero from "./Hero";
 import YearsList from "./YearsList";
+import ViewYearblock from "../../components/flow/get-user-yearblocks"
 
 function MyYearBlock() {
   return (
     <Layout>
       <Hero />
       <YearsList />
+      <ViewYearblock />
     </Layout>
   );
 }
