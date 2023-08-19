1e0ea19b359895f7e6a5ea4ee74c58faf83b02c9
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-08-19 16:57:45 +0500

    fixed build failed error

--- src/pages/minting.tsx
+++ src/pages/minting.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Layout from "components/Layout";
-import MintSignatureComponent from "components/flow/mint-signature";
 import AttachSignatureToYearBlockComponent from "components/flow/attach-signature";
 
 function Minting() {
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center gap-y-8 py-20">
-        <MintSignatureComponent className={"buttonPrimary"} />
         <AttachSignatureToYearBlockComponent className={"buttonPrimary"} />
       </div>
     </Layout>
