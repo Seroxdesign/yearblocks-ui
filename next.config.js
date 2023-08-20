67e2a12695f6633c341ad0161603f16e6a52ab10
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-08-20 23:46:41 +0500

    added header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["shared"],
+  headers: {
+    "/api/auth/(.*)": [
+      { key: "Access-Control-Allow-Origin", value: "*" },
+      { key: "Access-Control-Allow-Credentials", value: "true" },
+    ],
+  },
 };
 
 module.exports = nextConfig;
