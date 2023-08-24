ba13f4be1b0a1215c067b95fb404f6b5fd5e036c
MubashirLabar <61319180+MubashirLabar@users.noreply.github.com>
2023-08-24 21:19:37 +0500

    walletless create account

--- src/pages/api/accounts/create.ts
+++ src/pages/api/accounts/create.ts
@@ -5,13 +5,21 @@ import { loadFCLConfig } from "../../../utils/fcl-setup";
 loadFCLConfig();
 
 const createAccount = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "POST") {
-    res.status(405).send({ message: "Only POST requests allowed" });
-    return;
-  }
+  // console.log("req.....", req.method);
+  // if (req.method !== "POST") {
+  //   res.status(405).send({ message: "Only POST requests allowed" });
+  //   return;
+  // }
 
   const keys = await generateKeys();
   const address = await createAccountUtil(keys.publicKey);
+
+  console.log("address....", address);
+
+  // res.status(200).json({
+  //   message: "successfully!",
+  //   keys: keys,
+  // });
 
   res.status(200).json({
     publicKey: keys.publicKey,
